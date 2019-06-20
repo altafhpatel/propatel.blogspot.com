08c61101d5d7fa3454ac5975282299e416cbc64c
Altaf Hussain Patel <altafhpatel@users.noreply.github.com>
2019-06-20 13:01:21 +0500

    Update structure-data.js

--- structure-data.js
+++ structure-data.js
@@ -27,6 +27,7 @@ var data = {
  "description": "We love to do stuff to help people and stuff",
  "articleBody": "You can paste your entire post in here, and yes it can get really really long."
  };
+ console.log(data);
 var script = document.createElement('script');
 script.type = "application/ld+json";
 script.innerHTML = JSON.stringify(data);
