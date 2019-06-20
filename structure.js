bd04e8378b61e5163d06a9d7e9445506eab8eea1
Altaf Hussain Patel <altafhpatel@users.noreply.github.com>
2019-06-20 15:01:23 +0500

    Update structure.js

--- structure.js
+++ structure.js
@@ -5,17 +5,17 @@ var data = {
  "headline": Stitle,
  "image": Simage,
  "author": "Altaf Hussain Patel", 
- "genre": "search engine optimization", 
+ "genre": Sgenre, 
  "keywords": "seo sales b2b", 
  "wordcount": "1120",
-"publisher": {
-    "@type": "Organization",
-    "name": "Google",
-    "logo": {
-      "@type": "ImageObject",
-      "url": "https://google.com/logo.jpg"
-    }
-  },
+//"publisher": {
+   // "@type": "Organization",
+   // "name": "Google",
+   // "logo": {
+   //   "@type": "ImageObject",
+  //    "url": "https://google.com/logo.jpg"
+  //  }
+ // },
  "url": "http://www.example.com",
    "mainEntityOfPage": {
     "@type": "WebPage",
@@ -24,8 +24,8 @@ var data = {
  "datePublished": "2015-09-20",
  "dateCreated": "2015-09-20",
  "dateModified": "2015-09-20",
- "description": "We love to do stuff to help people and stuff",
- "articleBody": "You can paste your entire post in here, and yes it can get really really long."
+ "description": Sdescription,
+ "articleBody": Sbody
  };
  console.log(JSON.stringify(data));
 var script = document.createElement('script');
