dfaf5f052984021812b0e90fd5042d87dccb7d79
Altaf Hussain Patel <altafhpatel@users.noreply.github.com>
2019-06-26 10:48:36 +0500

    Create schemaa.js

---
+++ schemaa.js
@@ -0,0 +1,31 @@
+window.addEventListener('DOMContentLoaded', function() {
+var data = { 
+"@context": "https://schema.org", 
+ "@type": "Article",
+ "headline": Stitle,
+ "image": Simage,
+ "author": "Altaf Hussain Patel", 
+ "genre": Sgenre, 
+ "keywords": "seo sales b2b", 
+ "wordcount": Swords,
+"publisher": {
+    "@type": "Organization",
+    "name": "Pro Patel",
+   "logo": {
+      "@type": "ImageObject",
+     "url": "https://1.bp.blogspot.com/-LsQZq_7SrGE/XRHRqm1AGuI/AAAAAAAAA1Y/2jU64WWolgg0GPDB89ANYsKotWLcinOtgCLcBGAs/s1600/pro-patel-logo.jpg"
+    }
+  },
+ "url": Surl,
+ "datePublished": Spublished,
+ "dateCreated": Screated,
+ //"dateModified": "2015-09-20",
+ "description": Sdescription,
+ "articleBody": Sbody
+ };
+ console.log(JSON.stringify(data));
+var script = document.createElement('script');
+script.type = "application/ld+json";
+script.innerHTML = JSON.stringify(data);
+document.getElementsByTagName('head')[0].appendChild(script);
+ });
